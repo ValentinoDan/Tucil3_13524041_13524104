38d54d66fb361ce8bd0312f20fe74cb396a54c45
Daniel <19624107@mahasiswa.itb.ac.id>
2026-05-02 16:55:39 +0700

    feat: add board structure

--- src/puzzle/board.go
+++ src/puzzle/board.go
@@ -1,1 +1,14 @@
 package puzzle
+
+type Board struct {
+	N, M        int
+	Grid        [][]rune
+	Cost        [][]int
+	Start, Goal Point
+	Obstacle    map[int]Point
+	Lava        []Point
+}
+
+type Point struct {
+	Row, Col int
+}
