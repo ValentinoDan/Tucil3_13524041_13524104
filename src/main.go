2eb64d25b35097627f2e66140550883f48e1811f
Daniel <19624107@mahasiswa.itb.ac.id>
2026-05-03 11:05:33 +0700

    feat: add test and main to try it

--- src/main.go
+++ src/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"iceSlidingPuzzle/src/filehandler"
+)
+
+func main() {
+	board, err := filehandler.ParseBoard("test/test2.txt")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	board.Print()
+}
